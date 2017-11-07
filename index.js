5bcc72e719754a01c5a414b80a5344abce2b6c99
Malcolm Harris <harrismalcolm1@gmail.com>
2017-11-07 13:56:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function logWhisper(string){
   console.log('hello');
 }
 function sayHiToGrandma(string){
-  cosole.log("hello")
+  console.log("hello")
   return `I can\'t hear you!`;
 }
 function sayHiToGrandma(string){
