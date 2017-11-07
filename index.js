9e721ba95f1906c4214f86c98791d205bc89c017
Malcolm Harris <harrismalcolm1@gmail.com>
2017-11-07 14:22:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ function logWhisper(string){
 function sayHiToGrandma(string){
   var lowercase = "hello!"
   lowercase.toLowerCase() === lowercase;
-  return `I can\'t hear you!`
+  return `YES INDEED!`
 }
 function sayHiToGrandma(string){
   string.toUpperCase();
-  return `YES INDEED!`
+  return `I can\'t hear you!`
 }
