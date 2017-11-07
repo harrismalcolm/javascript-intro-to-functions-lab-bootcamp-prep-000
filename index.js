56d779a037ab23177fc0566400b49629cc599a18
Malcolm Harris <harrismalcolm1@gmail.com>
2017-11-07 14:10:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,12 +10,7 @@ function logShout(string){
 function logWhisper(string){
   console.log('hello');
 }
-function sayHiToGrandma(string){
-  if (string.toLowerCase){
-    return `I can\'t hear you!`;
-  } else if (string.toUpperCase) {
-    return `YES INDEED!`;
-  } else {
-    return `I love you, too.`
-  }
+function sayHiToGrandma('hello'){
+  return 'I can\"t hear you!'
 }
+
